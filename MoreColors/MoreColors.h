5cbc7c760b90b18efb519ab3c76941995c04a370
mccoyLBI <kyle.b.scholl@gmail.com>
2016-02-14 18:50:40 -0500

    Initial Commit

---
+++ MoreColors/MoreColors.h
@@ -0,0 +1,19 @@
+//
+//  MoreColors.h
+//  MoreColors
+//
+//  Created by Kyle Scholl on 2/14/16.
+//  Copyright © 2016 Patronus LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoreColors.
+FOUNDATION_EXPORT double MoreColorsVersionNumber;
+
+//! Project version string for MoreColors.
+FOUNDATION_EXPORT const unsigned char MoreColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoreColors/PublicHeader.h>
+
+
